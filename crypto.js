b314f36c1489e9e67dc06237e7720301e5d34f25
Annopaolo <arnaldo.cesco@gmail.com>
2020-06-16 18:19:42 +0200

    initial commit

---
+++ crypto.js
@@ -0,0 +1,3 @@
+const SimpleCrypto = require("simple-crypto-js").default
+const secretKey = "123456789oijhfgeR7W8EIHJGB3IUWYOFEPUWHG0W394JOEGPÀBHR93WA"
+const simpleCrypto = new SimpleCrypto(secretKey)
