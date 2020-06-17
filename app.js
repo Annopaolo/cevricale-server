b5c7abe4733f7fa57fc1e00dcc6fa5d1dbfc822b
Annopaolo <arnaldo.cesco@gmail.com>
2020-06-17 20:19:45 +0200

    Encrypted passwords
    And a more senseful API

--- app.js
+++ app.js
@@ -1,46 +1,106 @@
 const express = require('express')
-const app = express()
 var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+const app = express()
+
 const port = 3000
 
-const wrapper = require('./dbWrapper.js')
+//const wrapper = require('./dbWrapper.js')
 const crypto = require('./crypto.js')
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/therapist/:id', (req, res) => {
-  //console.log(req.params.id);
-  wrapper.getActivities(req.params.id, res)
-})
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const connectionUrl = "mongodb+srv://Annopaolo:I6CKXf5GeSkWu0H6@cluster0-fhh1o.mongodb.net/test?retryWrites=true&w=majority"
+// Database Name
+const dbName = 'cevricale-db';
 
-app.post('/login', (req, res) => {
-  const uid = req.body.id;
-  const psw = req.body.psw;
-  wrapper.checkCredentials(uid,psw, res);
-})
-
-
-app.get('/user', (req, res) => res.send('Hello, patient!'))
-
-app.post('/', (req,res) => {
-  wrapper.postActivity(req.body);
-  res.send("Ok!\n")
-})
-
-app.post('/newUser', (req,res) => {
-  const name = req.body.name;
-  const psw = req.body.psw;
-  wrapper.postTherapist(name, psw);
-  res.send("Ok!\n")
-})
-
-app.post('/newWatch', (req,res) => {
-  const uid = req.body.uid;
-  const thid = req.body.thid;
-  wrapper.postWatching(uid, thid);
-  res.send("Ok!\n")
-})
+MongoClient.connect(connectionUrl, { useUnifiedTopology: true })
+  .then(client => {
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-app.listen(port, console.log(`Example app listening at http://localhost:${port}`))
+    const db = client.db(dbName)
+
+    app.get('/', (req, res) => {
+      crypto.enc('Hello World!', res)
+    })
+
+    app.get('/therapist/:id', (req, res) => {
+      const activities = db.collection("activities");
+      const watching = db.collection("watching");
+      activities.aggregate([
+        {
+          $lookup:
+          {
+            from: "watching",
+            localField: "uid",
+            foreignField: "userId",
+            as: "associated_therapist"
+          }
+        },
+        {
+          $unwind:
+          {
+            path: "$associated_therapist",
+            preserveNullAndEmptyArrays: false
+          }
+        },
+        {
+          $match:
+          {
+            "associated_therapist.therapistId" : req.params.id
+          }
+        }
+      ]).toArray().then(result => {res.send(result);})
+    })
+
+    app.post('/login', (req, res) => {
+      const uid = req.body.name;
+      const psw = req.body.psw;
+      const therapist = db.collection("therapist");
+      const cursor = therapist.find({usn : uid}).toArray()
+      .then( (result) => {
+        if (result != "" && crypto.decSync(result[0].psw) == psw){
+          res.send("ok!");
+        } else {
+           res.status(403).send({ error: 'Forbidden!' });
+        }
+      })
+    })
+
+    app.post('/newTherapist', (req,res) => {
+      const name = req.body.name;
+      const psw = req.body.psw;
+      const therapist = db.collection("therapist");
+      therapist.find({usn: name}).toArray()
+      .then( (result) => {
+        if (result == "") {
+          therapist.insertOne({usn: name, psw: crypto.encSync(psw)});
+          res.send("ok!");
+        } else {
+          res.status(400).send({ error: 'Username already taken!' });
+        }
+    })})
+
+    app.post('/', (req,res) => {
+      const activities = db.collection("activities");
+      activities.insertOne({uid : req.uid, scene: req.scene, time: req.time, metric: req.metric, pain: req.pain, satisfaction: req.satisfaction});
+      res.send("Ok!\n")
+    })
+
+  app.post('/newWatch', (req,res) => {
+    const uid = req.body.uid;
+    const thid = req.body.thid;
+    const watching = db.collection("watching");
+    watching.insertOne({userId :uid, therapistId: thid});
+    res.send("Ok!\n")
+  })
+
+    app.get('/user', (req, res) => res.send('Hello, patient!'))
+
+    app.listen(port, console.log(`Example app listening at http://localhost:${port}`))
+
+
+  })
+  .catch(console.error)
--- crypto.js
+++ crypto.js
@@ -1,3 +1,40 @@
+var fs = require('fs')
+const util = require('util');
 const SimpleCrypto = require("simple-crypto-js").default
-const secretKey = "123456789oijhfgeR7W8EIHJGB3IUWYOFEPUWHG0W394JOEGPÀBHR93WA"
-const simpleCrypto = new SimpleCrypto(secretKey)
+
+const readFile = util.promisify(fs.readFile);
+
+const enc = (input, k) => {
+  readFile(".super-secret-file.k", 'utf8').then( (data) => k.send(encryptWithKey(data,input)));
+}
+
+const dec = (input, k) => {
+  readFile(".super-secret-file.k", 'utf8').then( (data) => k.send(decryptWithKey(data,input)));
+}
+
+const encSync = (input) => {
+  const key = fs.readFileSync(".super-secret-file.k", 'utf8');
+  return encryptWithKey(key, input);
+}
+
+const decSync = (input) => {
+  const key = fs.readFileSync(".super-secret-file.k", 'utf8');
+  return decryptWithKey(key, input);
+}
+
+const encryptWithKey = (key, plain) => {
+  const simpleCrypto = new SimpleCrypto(key);
+  const c = simpleCrypto.encrypt(plain);
+  return c;
+}
+
+const decryptWithKey = (key, cipher) => {
+  const simpleCrypto = new SimpleCrypto(key);
+  const m = simpleCrypto.decrypt(cipher);
+  return m;
+}
+
+exports.decSync = decSync
+exports.encSync = encSync
+exports.enc = enc
+exports.dec = dec
